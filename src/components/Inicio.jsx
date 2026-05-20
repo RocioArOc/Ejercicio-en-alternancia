b41a06a6cc4dbf0da9d8dab877dc951c49e77121
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2026-05-20 23:23:59 +0200

    Prueba de despliegue automático en Vercel

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -5,6 +5,11 @@ import './Inicio.css';
 export default function Inicio() {
   return (
     <div className="contenedor-pagina">
+      <p>Esto es una prueba de actualización en Vercel</p>
+
+
+
+
       {/* SISTEMA DE POSTS DINÁMICOS / CMS BLOG */}
       <Blog />
 
